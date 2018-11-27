03767d227597edddcbb7cccd559b03554826cc6a
Davide <davide.zanella10@gmail.com>
2018-11-27 11:21:14 +0100

    added implementation with filters

--- db/answers_db.js
+++ db/answers_db.js
@@ -1,8 +1,9 @@
 let db = require('./db');
 
 let answers_db = {
-    getAllAnswers: async function(){
-        let res = await db.executeQuery('SELECT id FROM answers;');
+    getAllAnswers: async function(user_id, task_id, type){
+        let res = await db.executeQuery('SELECT answers.id FROM answers JOIN tasks ON task_id = tasks.id '
+            + 'WHERE user_id::text LIKE $1 AND task_id::text LIKE $2 AND type::text LIKE $3;', [user_id, task_id, type]);
 
         return res.rows.map((x) => {
             return x.id;
--- routes/answers.js
+++ routes/answers.js
@@ -4,10 +4,14 @@ var router = express.Router();
 let answers_logic = require('../logic/answers_logic');
 
 router.get('/answers', (req, res) => {
-    answers_logic.getAllAnswers()
+    let user_id = req.query.user_id; 
+    let task_id = req.query.task_id; 
+    let type = req.query.type;
+
+    answers_logic.getAllAnswers(user_id, task_id, type)
         .then(data => res.json(data))
         .catch(e => {
-            res.status(400).send(e.message);
+            res.status(404).send(e.message);
             console.log(e.stack);
         });
 });
