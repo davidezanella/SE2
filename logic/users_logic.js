9ca1136390ec7a706547ced531b9486367b7f291
Andrea Dalla Costa <andrea.dallacosta@studenti.unitn.it>
2018-11-28 00:22:29 +0100

    create /users POST and GET functions

--- logic/users_logic.js
+++ logic/users_logic.js
@@ -2,6 +2,10 @@ const isNumber = require('is-number');
 let users_db = require('../db/users_db');
 
 let users = {
+  getAllUsers: async function (name, surname, email) {
+  },
+  createNewUser: async function(username, name, surname, email) {
+  }
 };
 
 module.exports = users;
