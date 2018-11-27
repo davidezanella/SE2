d4989c4b8c6ca1113eeea2f4b816629e27f29f67
Sabrina Alexandra Rosso <sabrina.rosso@studenti.unitn.it>
2018-11-27 22:21:24 +0100

    Insert a task implementation

--- db/tasks_db.js
+++ db/tasks_db.js
@@ -1,6 +1,24 @@
 let db = require('./db');
 
 let tasks_db = {
+    getAllTasks: async function(){
+        let res = await db.executeQuery('SELECT id FROM tasks;');
+
+        return res.rows.map((x) => {
+            return x.id;
+        });
+    },
+    insertTask: async function(task){
+        let res = await db.executeQuery('INSERT INTO tasks (title, author, question, type) VALUES ($1, $2, $3, $4) RETURNING id', [task.task_title, task.author_id, task.question, task.task_type]);
+
+        let  id = res.rows[0].id;
+
+        for(let i=0; i<task.choices.length; i++){
+            let res = await db.executeQuery('INSERT INTO task_choices (task_id, choice, correct) VALUES ($1, $2, $3)', [id, task.choices[i], task.correct_answer.includes(task.choices[i]) ]);
+        };
+
+        return res.rows[0].id;
+    }
 };
 
 module.exports = tasks_db;
--- logic/tasks_logic.js
+++ logic/tasks_logic.js
@@ -1,7 +1,56 @@
 const isNumber = require('is-number');
+const isString = require('is-string');
 let tasks_db = require('../db/tasks_db');
 
 let tasks = {
+    insertTask: async function (task) {
+        let types = ["multi_choice", "single_choice", "open_answer", "true_false"];
+
+        // Check task 
+        if (!(typeof task === 'object'))
+            throw new Error("Task is not valid.");
+        // Check title type 
+        if (!isString(task.task_title))
+            throw new Error("Title is not valid!");
+        // Check author type 
+        if (!isNumber(task.author_id))
+            throw new Error("Author ID is not valid!");
+        // Check question type 
+        if (!isString(task.question))
+            throw new Error("Question is not valid!");
+        // Check choices type
+        if (!Array.isArray(task.choices))
+            throw new Error("Choices are not valid!");
+        // Check correct answers type
+        if (!Array.isArray(task.correct_answer))
+            throw new Error("Coorrect answers are not valid!");
+
+        // Check task type 
+        let tmp = false;
+        for(let i=0; i<types.length; i++){
+            if (task.task_type === types[i]) 
+                tmp = true;
+        }
+        if(!tmp) throw new Error("Task type is not valid!");
+
+        // Check choices
+        if (task.choices.length === 0)
+            throw new Error("No choices added.");
+
+        // Check correct answers
+        if (task.correct_answer.length === 0)
+            throw new Error("No correct answers added.");
+
+        if(tasks.length === 0)
+            throw new Error("No tasks added!");
+        
+        return tasks_db.insertTask(task);
+    },
+
+
+    getAllTasks: async function (task_title, author_id, question, task_type) {
+        return tasks_db.getAllTasks();
+    }
 };
 
 module.exports = tasks;
