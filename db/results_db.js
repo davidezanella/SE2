04ba8883ff83526e57be725488b6977d0b201adf
Andrea Dalla Costa <andrea.dallacosta@studenti.unitn.it>
2018-12-13 03:57:06 +0100

    add get /results routes and db query

--- db/results_db.js
+++ db/results_db.js
@@ -1,6 +1,27 @@
 let db = require('./db');
 
 let results_db = {
+  getAllResults: async function(exam, user) {
+    let query = 'SELECT results.id FROM results ';
+    let params = [];
+    if (exam !== null) {
+      params.push(exam);
+      query = query + " WHERE exam_id = $1 ";
+    }
+    if (user !== null) {
+      params.push(user);
+      if (params.length == 2)
+        query = query + " AND user_id = $2 ";
+      else
+        query = query + " WHERE user_id = $1 ";
+    }
+    query += ";";
+    let res = await db.executeQuery(query, params);
+
+    return res.rows.map((x) => {
+      return x.id;
+    });
+  },
 };
 
 module.exports = results_db;
--- routes/results.js
+++ routes/results.js
@@ -4,7 +4,15 @@ var router = express.Router();
 let results_logic = require('../logic/results_logic');
 
 router.get('/results', (req, res) => {
-    res.send("ok");
+  // Optionally apply filters by exam and user.
+  let exam = req.query.exam_id;
+  let user = req.query.user_id;
+
+  results_logic.getAllResults(exam, user)
+      .then(data => res.json(data))
+      .catch(e => {
+          res.status(404).send(e.message);
+      });
 });
 
 module.exports = router;
