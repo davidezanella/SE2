59e55f7f2fa3b02c426aef1bc374973b900ab788
Andrea Dalla Costa <andrea.dallacosta@studenti.unitn.it>
2018-12-13 04:00:17 +0100

    add /results logic implementation. It'll need more tests when exams code is ready

--- logic/results_logic.js
+++ logic/results_logic.js
@@ -2,6 +2,19 @@ const isNumber = require('is-number');
 let results_db = require('../db/results_db');
 
 let results = {
+  //TODO: add tests when exams code is ready
+  getAllResults: async function(exam, user) {
+    if (exam === undefined || exam === null)
+        exam = null;
+    else if (!isNumber(exam))
+        throw new Error("Invalid type of the exam filter parameter!");
+    if (user === undefined || user === null)
+        user = null;
+    else if (!isNumber(user))
+        throw new Error("Invalid type of the user filter parameter!");
+
+    return await results_db.getAllResults(exam, user);
+  }
 };
 
 module.exports = results;
